d7ea522d9e6cda5d6d361cb186f439e558a751d7
Eric Rogness <ericrogness@hotmail.com>
2012-05-12 14:35:55 -0400

    Almost done signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SampleApp::Application.routes.draw do
-get "users/new"
+resources :users
 
 root				to: 'static_pages#home'
 
@@ -8,6 +8,8 @@ match '/signup',	to: 'users#new'
 match '/help', 		to: 'static_pages#help'
 match '/about',		to: 'static_pages#about'
 match '/contact',	to: 'static_pages#contact'
+
+
 
 
   # The priority is based upon order of creation:
